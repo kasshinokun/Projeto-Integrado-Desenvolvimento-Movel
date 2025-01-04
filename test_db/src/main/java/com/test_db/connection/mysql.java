2595a87f179fd2691959898d00050fc09fde42b0
Gabriel Cassino <120288615+kasshinokun@users.noreply.github.com>
2025-01-04 12:07:15 -0300

    application java 04-01-2025

---
+++ test_db/src/main/java/com/test_db/connection/mysql.java
@@ -0,0 +1,15 @@
+package com.test_db.connection;
+
+/**
+ * @version 1_2025_01_04
+ * @author Gabriel da Silva Cassino
+ */
+public class mysql {
+    public static void main(int i) {
+        if (i==1){
+            System.out.println("English-USA");
+        }else{
+            System.out.println("Portugues-Brasil");
+        } 
+    }    
+}
