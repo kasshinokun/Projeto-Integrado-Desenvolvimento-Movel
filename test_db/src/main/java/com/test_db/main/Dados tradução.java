c043c441197055587d507d5f86f0d55e71ebd9bf
Gabriel Cassino <120288615+kasshinokun@users.noreply.github.com>
2025-01-05 11:11:43 -0300

    Dados tradução.java の更新

--- test_db/src/main/java/com/test_db/main/Dados tradução.java
+++ test_db/src/main/java/com/test_db/main/Dados tradução.java
@@ -1,5 +1,18 @@
 package com.test_db.main;
 
+
+/*
+Need some adaptations for Code in this project
+
+Created by Rodrigo Henriques da Silva Leone
+
+Created on 15/06/2024 
+
+Source-base URL: https://cursos.alura.com.br/forum/topico-projeto-alternativa-de-api-para-traducao-433467
+
+
+*/
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
