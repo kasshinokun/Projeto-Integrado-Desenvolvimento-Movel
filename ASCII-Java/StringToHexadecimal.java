078d2f9fbe1092d641901570cac4119978b8561e
Gabriel Cassino <120288615+kasshinokun@users.noreply.github.com>
2025-02-15 07:46:53 +0900

    Update StringToHexadecimal.java

--- ASCII-Java/StringToHexadecimal.java
+++ ASCII-Java/StringToHexadecimal.java
@@ -1,4 +1,45 @@
 import java.util.*;
+import java.util.stream.Collectors;
+
+public class StringToBinaryExample1 {
+
+    public static void StringToBinaryExample1(String input) {
+      
+        String result = convertStringToBinary(input);
+
+        System.out.println(result);
+
+        // pretty print the binary format
+        System.out.println(prettyBinary(result, 8, " "));
+
+    }
+
+    public static String convertStringToBinary(String input) {
+
+        StringBuilder result = new StringBuilder();
+        char[] chars = input.toCharArray();
+        for (char aChar : chars) {
+            result.append(
+                    String.format("%8s", Integer.toBinaryString(aChar))   // char -> int, auto-cast
+                            .replaceAll(" ", "0")                         // zero pads
+            );
+        }
+        return result.toString();
+
+    }
+
+    public static String prettyBinary(String binary, int blockSize, String separator) {
+
+        List<String> result = new ArrayList<>();
+        int index = 0;
+        while (index < binary.length()) {
+            result.add(binary.substring(index, Math.min(index + blockSize, binary.length())));
+            index += blockSize;
+        }
+
+        return result.stream().collect(Collectors.joining(separator));
+    }
+}
 
 /*
 Source-base developed by Tutorialspoint
