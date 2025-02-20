27d4ef476124245be7f1fbd0d96e6a07a36c1e68
Gabriel Cassino <120288615+kasshinokun@users.noreply.github.com>
2025-02-21 05:27:35 +0900

    Preparação para update 1_21_02_2025

--- Flutther_Dart_Android/App_Rent_House/project_1/Sprint 1/TP_S1/lib/Pages/App.dart
+++ Flutther_Dart_Android/App_Rent_House/project_1/Sprint 1/TP_S1/lib/Pages/App.dart
@@ -1,1 +1,17 @@
+//Preparação para update 1_21_02_2025
+
+import 'package:flutter/material.dart'; 
+import 'package:rent_a_house/Pages/Start/welcome.dart'
+
+class MyApp extends StatelessWidget { 
+@override 
+Widget build(BuildContext context) { 
+	return MaterialApp( 
+	debugShowCheckedModeBanner: false, 
+	home: WelcomePage(), 
+	); 
+} 
+} 
+
+
 
