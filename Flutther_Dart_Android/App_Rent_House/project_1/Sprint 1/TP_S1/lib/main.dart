f1b5564d4f87b329b8fea98c43b48f0598865588
Gabriel Cassino <120288615+kasshinokun@users.noreply.github.com>
2025-02-21 09:59:35 -0300

    Update 1_21_02_2025

--- Flutther_Dart_Android/App_Rent_House/project_1/Sprint 1/TP_S1/lib/main.dart
+++ Flutther_Dart_Android/App_Rent_House/project_1/Sprint 1/TP_S1/lib/main.dart
@@ -1,10 +1,9 @@
-//Preparação para update 1_21_02_2025
-
+//Update 1_21_02_2025
 import 'package:flutter/material.dart';
+import 'package:rent_house/Pages/start.dart';
 
-
-import 'package:rent_a_house/lib/Pages/Start.dart' //Classe do Programa 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp()); //Inicializa a aplicação
 }
+
 
