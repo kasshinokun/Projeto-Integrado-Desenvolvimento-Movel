77d872fc6155451ebfb0d842e00bcbaa25013ce9
Gabriel Cassino <120288615+kasshinokun@users.noreply.github.com>
2025-03-11 08:19:00 +0900

    Create create_app_folder.bat

---
+++ Rent_a_House_App/Exemplos/create_app_folder.bat
@@ -0,0 +1,18 @@
+
+PAUSE ---------Indo pasta a pasta documentos 
+
+
+
+PAUSE ---------Criando pasta
+
+
+
+PAUSE ---------Criando projeto 
+
+
+
+PAUSE ---------Entrando na pasta
+
+.code .\
+
+PAUSE ---------Processo Finalizado
