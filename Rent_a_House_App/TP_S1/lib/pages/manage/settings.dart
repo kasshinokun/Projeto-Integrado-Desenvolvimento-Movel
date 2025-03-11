86ad7975dc30f35250ff073241d1028786d5ebfe
Gabriel Cassino <120288615+kasshinokun@users.noreply.github.com>
2025-03-11 13:51:18 -0300

    Update 1_11_03_2025

--- Rent_a_House_App/TP_S1/lib/pages/manage/settings.dart
+++ Rent_a_House_App/TP_S1/lib/pages/manage/settings.dart
@@ -36,101 +36,138 @@ class _SettingsScreen extends State<SettingsScreen> {
               ),
         ), // Fim do Icone Home
       ), // Fim do AppBar
-      body: Column(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ExpansionTile(
+              title: Text(
+                "Geral",
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              leading: Icon(Icons.electrical_services_rounded),
+              controlAffinity: ListTileControlAffinity.leading,
+              children: <Widget>[polities(context)],
+            ),
+            SizedBox(height: 12),
+            ExpansionTile(
+              title: Text("Idioma"),
+              trailing: Icon(Icons.arrow_forward_ios),
+              leading: Icon(Icons.language_rounded),
+              controlAffinity: ListTileControlAffinity.leading,
+              children: <Widget>[polities(context)],
+            ),
+            ExpansionTile(
+              title: Text("Segurança"),
+              trailing: Icon(Icons.arrow_forward_ios),
+              leading: Icon(Icons.security),
+              controlAffinity: ListTileControlAffinity.leading,
+              children: <Widget>[polities(context)],
+            ),
+            ListTile(
+              title: Text("Buscar MyHouses"),
+              trailing: Icon(Icons.rss_feed),
+              leading: Icon(Icons.bluetooth_outlined),
+              onTap: () {
+                //
+              },
+            ),
+            SizedBox(height: 12),
+            ExpansionTile(
+              title: Text(
+                "Ajuda & Suporte",
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              leading: Icon(Icons.electrical_services_rounded),
+              controlAffinity: ListTileControlAffinity.leading,
+              children: <Widget>[polities(context)],
+            ),
+            SizedBox(height: 12),
+            ExpansionTile(
+              title: Text("Sobre"),
+              trailing: Icon(Icons.arrow_forward_ios),
+              leading: Icon(Icons.info_outline_rounded),
+              controlAffinity: ListTileControlAffinity.leading,
+              children: <Widget>[polities(context)],
+            ),
+            SizedBox(height: 12),
+            ExpansionTile(
+              title: Text(
+                "Privacidade",
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              leading: Icon(Icons.electrical_services_rounded),
+              controlAffinity: ListTileControlAffinity.leading,
+              children: <Widget>[polities(context)],
+            ),
+            SizedBox(height: 12),
+            ExpansionTile(
+              title: Text("Termos de Serviço"),
+              trailing: Icon(Icons.arrow_forward_ios),
+              leading: Icon(Icons.electrical_services_rounded),
+              controlAffinity: ListTileControlAffinity.leading,
+              children: <Widget>[polities(context)],
+            ),
+            ExpansionTile(
+              title: Text("Politicas de Privacidade"),
+              trailing: Icon(Icons.arrow_forward_ios),
+              leading: Icon(Icons.privacy_tip_rounded),
+              controlAffinity: ListTileControlAffinity.leading,
+              children: <Widget>[polities(context)],
+            ),
+            ExpansionTile(
+              title: Text("Politicas de Segurança"),
+              trailing: Icon(Icons.arrow_forward_ios),
+              leading: Icon(Icons.policy),
+              controlAffinity: ListTileControlAffinity.leading,
+              children: <Widget>[polities(context)],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget polities(context) {
+    return Container(
+      padding: EdgeInsets.all(8),
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          Text('Mission Statement:'),
+          SizedBox(height: 8),
           Text(
-            "Geral",
-            style: TextStyle(
-              fontSize: 24,
-              color: Colors.black,
-              fontWeight: FontWeight.w500,
-            ),
+            'Our mission is to provide high-quality products and services to our customers.',
           ),
-          SizedBox(height: 12),
+          SizedBox(height: 16),
+          Text('Values:'),
+          SizedBox(height: 8),
           ListTile(
-            title: Text("Idioma"),
-            trailing: Icon(Icons.arrow_forward_ios),
-            leading: Icon(Icons.language_rounded),
-            onTap: () {
-              //
-            },
+            leading: Icon(Icons.check),
+            title: Text('Customer satisfaction is our top priority'),
           ),
           ListTile(
-            title: Text("Segurança"),
-            trailing: Icon(Icons.arrow_forward_ios),
-            leading: Icon(Icons.security),
-            onTap: () {
-              //
-            },
+            leading: Icon(Icons.check),
+            title: Text('We strive for continuous improvement'),
           ),
           ListTile(
-            title: Text("Buscar MyHouses"),
-            trailing: Icon(Icons.rss_feed),
-            leading: Icon(Icons.bluetooth_outlined),
-            onTap: () {
-              //
-            },
-          ),
-          SizedBox(height: 12),
-          Text(
-            "Ajuda & Suporte",
-            style: TextStyle(
-              fontSize: 24,
-              color: Colors.black,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-          SizedBox(height: 12),
-          ListTile(
-            title: Text("Sobre"),
-            trailing: Icon(Icons.arrow_forward_ios),
-            leading: Icon(Icons.info_outline_rounded),
-            onTap: () {
-              //
-            },
-          ),
-          SizedBox(height: 12),
-          Text(
-            "Privacidade",
-            style: TextStyle(
-              fontSize: 24,
-              color: Colors.black,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-          SizedBox(height: 12),
-          ListTile(
-            title: Text("Termos de Serviço"),
-            trailing: Icon(Icons.arrow_forward_ios),
-            leading: Icon(Icons.electrical_services_rounded),
-            onTap: () {
-              //
-            },
-          ),
-          ListTile(
-            title: Text("Politicas de Privacidade"),
-            trailing: Icon(Icons.arrow_forward_ios),
-            leading: Icon(Icons.privacy_tip_rounded),
-            onTap: () {
-              //
-            },
-          ),
-          ListTile(
-            title: Text("Politicas de Segurança"),
-            trailing: Icon(Icons.arrow_forward_ios),
-            leading: Icon(Icons.policy),
-            onTap: () {
-              //
-            },
+            leading: Icon(Icons.check),
+            title: Text('We value honesty and integrity in all our actions'),
           ),
         ],
       ),
     );
   }
-
-  //Processo final de Busca
-  //
-  //
-  //
 }
