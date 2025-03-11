9f3a16b12cd02b27ac8516a3bc3ac9d1fc1a35c6
Gabriel Cassino <120288615+kasshinokun@users.noreply.github.com>
2025-03-11 11:38:12 +0900

    Update create_app_folder.bat

--- Rent_a_House_App/Exemplos/create_app_folder.bat
+++ Rent_a_House_App/Exemplos/create_app_folder.bat
@@ -1,18 +1,22 @@
 
 PAUSE ---------Indo pasta a pasta documentos 
 
+set USERDOCS=%USERPROFILE%\Documents
 
+cd %USERDOCS%
 
 PAUSE ---------Criando pasta
 
+mkdir TP_S1
 
+cd TP_S1
 
 PAUSE ---------Criando projeto 
 
-
+flutter create --org com.grupo.rentahouse --platform android,web,windows --project-name rent_a_house ./
 
 PAUSE ---------Entrando na pasta
 
-.code .\
+code .\
 
 PAUSE ---------Processo Finalizado
