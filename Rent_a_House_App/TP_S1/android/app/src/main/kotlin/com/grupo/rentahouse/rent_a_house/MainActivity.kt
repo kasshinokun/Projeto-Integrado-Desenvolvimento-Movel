075c1feac62b5e22df27d59855fa05d3e3ce017f
Gabriel Cassino <120288615+kasshinokun@users.noreply.github.com>
2025-03-24 16:28:57 -0300

    Delete Rent_a_House_App/TP_S1 directory

--- Rent_a_House_App/TP_S1/android/app/src/main/kotlin/com/grupo/rentahouse/rent_a_house/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.grupo.rentahouse.rent_a_house
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- Rent_a_House_App/TP_S1/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:rent_a_house/pages/start.dart';
-
-void main() {
-  runApp(const MyApp());
-}
