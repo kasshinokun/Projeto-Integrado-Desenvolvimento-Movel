cfc149c76a3fe654135a337be54149a83607abbc
Gabriel Cassino <120288615+kasshinokun@users.noreply.github.com>
2025-04-03 07:38:00 +0900

    Create customsearchdelegate.dart

---
+++ Rent_a_House_App/Releases/TP_S2/lib/pages/s1/pages/manage/customsearchdelegate.dart
@@ -0,0 +1,100 @@
+
+
+
+class CustomSearchDelegate extends SearchDelegate<String>{
+  //Buscará em uma lista(pode ser List<Strings>) => addressItens
+
+  //---> Objetivos em aberto 
+  //Deixar searchdelegate transparente sem destruir a página anterior
+  //---> Fim dos objetivos em aberto 
+  
+  //Customizando o SearchDelegate
+  CustomSearchDelegate({this.hintText}): super(
+          //searchFieldLabel: hintText, // Descomente em caso derro
+          keyboardType: TextInputType.text,
+          searchFieldStyle: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold),
+        );
+  
+  ///* Em caso de erro, deixe este trecho comentado
+  @override
+  String? get searchFieldLabel => hintText;
+  //*/Fim do trecho 
+  
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      ElevatedButton.icon(
+        label: Text(
+          "Limpar",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        onPressed: () {
+          query = '';
+        },
+        icon: Icon(Icons.clear_rounded),
+      ),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        close(context, null);
+      },
+      icon: Icon(Icons.arrow_back),
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    List<String> matchQuery = []; //Coleta os resultados
+    for (var address in addressItens) {
+      if (address.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(address);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(title: Text(result));
+      },
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> matchQuery = []; //Coleta os resultados
+    for (var address in addressItens) {
+      if (address.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(address);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(title: Text(result));
+      },
+    );
+  }
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//================================================================================    
+    
