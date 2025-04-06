e5772e9c133fc61e60b10fe4121d3395a540c172
Gabriel Cassino <120288615+kasshinokun@users.noreply.github.com>
2025-04-07 04:11:42 +0900

    Create viacep.dart

---
+++ Rent_a_House_App/Releases/TP_S2/lib/pages/cep/viacep.dart
@@ -0,0 +1,15 @@
+
+
+import 'package:http/http.dart' as http;
+import 'package:rent_a_house/pages/cep/resultcep.dart';
+
+class ViaCepService {
+  static Future<ResultCep> fetchCep({String cep}) async {
+    final response = await http.get('https://viacep.com.br/ws/$cep/json/');
+    if (response.statusCode == 200) {
+      return ResultCep.fromJson(response.body);
+    } else {
+      throw Exception('Requisição inválida!');
+    }
+  }
+}
