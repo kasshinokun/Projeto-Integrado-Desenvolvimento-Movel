f841922f68230fdc16abad0841415e6f1927d247
Gabriel Cassino <120288615+kasshinokun@users.noreply.github.com>
2025-04-07 04:15:50 +0900

    Create viacep.dart

--- Rent_a_House_App/Releases/TP_S2/lib/pages/cep/viacep.dart
+++ Rent_a_House_App/Releases/TP_S2/lib/pages/cep/viacep.dart
@@ -1,3 +1,9 @@
+//Código desenvolvido por Kleber Andrade - Setembro 12, 2019
+//Adaptado por Gabriel Cassino - Abril 06, 2025
+//Source-base URL: https://medium.com/flutter-comunidade-br/consultando-ceps-com-flutter-a395b86ce34a
+
+//Rode antes: flutter pub add http
+//import 'package:http/http.dart'; //se precisar, descomente este import
 
 
 import 'package:http/http.dart' as http;
