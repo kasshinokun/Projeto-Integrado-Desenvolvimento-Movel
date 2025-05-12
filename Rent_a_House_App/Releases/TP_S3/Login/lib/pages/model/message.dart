dec398bc65d6e1bf2616756e407950060a5fda24
Gabriel Cassino <120288615+kasshinokun@users.noreply.github.com>
2025-05-13 08:08:25 +0900

    Update 2.1-12-05-2025

--- Rent_a_House_App/Releases/TP_S3/Login/lib/pages/model/message.dart
+++ Rent_a_House_App/Releases/TP_S3/Login/lib/pages/model/message.dart
@@ -1,3 +1,7 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+//Model
 class Message{
   int? id;
   String? senderMessage;
@@ -5,7 +9,96 @@ class Message{
   String? bodyMessage;
   DateTime? dateMessage;
 }
+
+//DAO
 class MessageDBHelper {
+  MessageDBHelper._();
+
+  static const tableName = 'messagesUsers';
+
+  static const columnId: 'id';
+  static const columnSenderMessage: 'senderMessage';
+  static const columnReceiverMessage: 'receiverMessage';
+  static const columnBodyMessage: 'bodyMessage';
+  static const columnDateMessage: 'dateMessage';
+            
+  static final MessageDBHelper instance = MessageDBHelper._();
+
+  static const _dbName = 'rentahouse.db';
+  static const _dbVersion = 1;
+
+  Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) {
+      return _database!;
+    }
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    String directory = await getDatabasesPath();
+    String path = join(directory, _dbName);
+    return openDatabase(
+      path,
+      version: _dbVersion,
+      onCreate: _onCreate,
+      onConfigure: _onConfigure,
+    );
+  }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE $tableName (
+      $columnId               INTEGER PRIMARY KEY AUTOINCREMENT,
+      $columnSenderMessage    TEXT    NOT NULL,
+      $columnReceiverMessage  TEXT    NOT NULL,
+      $columnBodyMessage      TEXT    NOT NULL,
+      $columnDateMessage      INTEGER NOT NULL,)
+      ''');
+  }
+
+  Future _onConfigure(Database db) async {
+    await db.execute('PRAGMA foreign_keys = ON');
+  }
+
+  Future<int> insert(Map<String, dynamic> row) async {
+    Database db = await instance.database;
+    return db.insert(tableName, row);
+  }
+
+  Future<List<Map<String, dynamic>>> queryAll() async {
+    Database db = await instance.database;
+    return db.query(tableName);
+  }
+
+  Future<Map<String, dynamic>> queryById(int id) async {
+    Database db = await instance.database;
+    List<Map<String, dynamic>> results =
+        await db.query(tableName, where: '$columnId = ?', whereArgs: [id]);
+
+    return results.single;
+  }
+
+  Future<int> update(Map<String, dynamic> row) async {
+    Database db = await instance.database;
+    return db.update(
+      tableName,
+      row,
+      where: '$columnId = ?',
+      whereArgs: [row[columnId]],
+    );
+  }
+
+  Future<int> delete(int id) async {
+    Database db = await instance.database;
+    return db.delete(tableName, where: '$columnId = ?', whereArgs: [id]);
+  }
+}
+
+//DAO Helper
+class MessageHelper {
   final MessageDBHelper _dbHelper = MessageDBHelper.instance;
 
   Future<int> insert(Message message) async {
@@ -40,7 +133,7 @@ class MessageDBHelper {
     };
   }
 
-  Report fromMap(Map<String, dynamic> map) {
+  Message fromMap(Map<String, dynamic> map) {
     return Report()
       ..id = map[MessageDBHelper.columnId] as int
       ..senderMessage = map[MessageDBHelper.columnSenderMessage] as String
