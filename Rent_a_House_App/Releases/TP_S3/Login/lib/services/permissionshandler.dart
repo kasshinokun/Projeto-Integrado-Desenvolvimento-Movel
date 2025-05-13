beec20c40e2c476e1fd32f5c8b5b83b675659bb0
Gabriel Cassino <120288615+kasshinokun@users.noreply.github.com>
2025-05-13 11:10:13 +0900

    Update 2.2-12-05-2025

--- Rent_a_House_App/Releases/TP_S3/Login/lib/services/permissionshandler.dart
+++ Rent_a_House_App/Releases/TP_S3/Login/lib/services/permissionshandler.dart
@@ -1,6 +1,57 @@
 
 //Teste de permissão em tempo de execução 
+import 'package:permission_handler/permission_handler.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  PermissionStatus _cameraPermission = PermissionStatus.denied;
+
+  Future<void> _checkPermission() async {
+    _cameraPermission = await Permission.camera.status;
+    if (_cameraPermission != PermissionStatus.granted) {
+      // 許可がされていない場合
+      var status = await Permission.camera.request();
+      setState(() {
+        _cameraPermission = status;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Permission Example'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Permissão de câmera: ${_cameraPermission}'),
+              ElevatedButton(
+                onPressed: () async {
+                  await _checkPermission();
+                },
+                child: Text('Permissão de câmera - checagem'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//Bluetooth -----> carece análise 
 Future<void> _checkPermissionBluetooth() async {
     DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
     final androidInfo = await deviceInfoPlugin.androidInfo;
